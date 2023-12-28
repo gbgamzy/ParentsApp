0bfddea8b6b820c11b88bb4c6f0d40b09a55292b
Gautam Bhateja <bhatejagautam1@gmail.com>
2023-12-29 03:00:54 +0530

    Major changes

---
+++ server_node/ama.js
@@ -0,0 +1,139 @@
+
+// var url = "https://www.emigaps.co.in/api/intro/";
+var url = "http://127.0.0.1:8003/AMA/";
+
+const http = require("http");
+const axios = require("axios");
+
+const policyPrefix = "enterprises/LC03rv38l8/policies/"
+
+var options = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        
+    }
+};
+
+var defaultPolicy = {
+    applications: [],
+    adjustVolumeDisabled: false,
+    installAppsDisabled: false,
+    factoryResetDisabled: true,
+    mountPhysicalMediaDisabled: false,
+    outgoingCallsDisabled: false,
+    usbFileTransferDisabled: false,
+    bluetoothDisabled: false,
+    playStoreMode: "BLACKLIST",
+    advancedSecurityOverrides: {
+        "untrustedAppsPolicy": "DISALLOW_INSTALL",
+        "developerSettings": "DEVELOPER_SETTINGS_DISABLED"
+    },
+    locationMode: "LOCATION_USER_CHOICE"
+}
+
+
+async function getEnrollmentToken(name) {
+    name = policyPrefix + name;
+    var policyItself = defaultPolicy;
+    policyItself['name'] = name;
+    var requestBody = JSON.stringify({
+        "policyItself": policyItself
+    });
+    try {
+        var r = null;
+        await axios.post(url + 'getEnrollmentToken1/', requestBody, options)
+            .then((res) => {
+                console.log(res.data.body['token']['qrCode']);
+            if (res.status == 200){
+                r = res.data.body['token']['qrCode'];
+            }
+            else {
+                console.log(res.data);
+            }
+            
+            });
+        return r;
+    }
+    catch (e) {
+        console.log(e);
+
+    }
+        
+
+
+}
+
+async function createPolicy(name) {
+    name = policyPrefix + name;
+    var policyItself = defaultPolicy;
+    policyItself['name'] = name;
+    var requestBody = JSON.stringify({
+        "policyItself": policyItself
+    });
+    try {
+        await axios.put(url + 'updatePolicy1/', requestBody, options)
+        .then((res) => {
+            if (res.status == 200){
+                return true;
+            }
+            else {
+                console.log(res.data);
+                return false;
+            }
+            
+        });
+    }
+    catch (e) {
+        console.log(e);
+
+    }
+}
+
+async function updatePolicy(policy) {
+    var options1 = {
+        headers: {
+            "Content-Type": "application/json",
+        }
+    };
+
+    var requestBody = JSON.stringify({
+        "policyItself": policy
+    });
+    console.log("Request body")
+    console.log(policy);
+    console.log(policy.policyItself.advancedSecurityOverrides)
+    delete policy.policyItself.advancedSecurityOverrides;
+
+    try {
+        await axios.put(url + 'updatePolicy1/', policy
+            // , options1
+        )
+            .then((res) => {
+                console.log("Printing Response")
+                console.log(res.data);
+            if (res.status == 200){
+                return true;
+            }
+            else {
+                // console.log(res.data);
+                return false;
+            }
+            
+        });
+    }
+    catch (e) {
+        console.log("Error from axios");
+        console.log(e)
+        return false;
+    }
+}
+
+
+module.exports = {
+    policyPrefix: policyPrefix,
+    getEnrollmentToken: getEnrollmentToken,
+    createPolicy: createPolicy,
+    updatePolicy: updatePolicy,
+    policyPrefix: policyPrefix
+}
---
+++ server_node/subscription.js
@@ -0,0 +1,96 @@
+var http = require('http');
+var express = require('express');
+var events = require('events');
+var axios = require('axios');
+
+const { policyPrefix } = require('./ama');
+
+const { Value, Image, User, Device, Policy } = require('./model');
+
+
+const subscriptionId = 'projects/emisafe/subscriptions/DeviceEnrolledSubscription';
+const subscriptionName = 'EMIGAPS_SUBSCRIPTION';
+const timeout = 100n * 365n * 24n * 60n * 60n * 1000n; // 100    years in milliseconds
+
+
+// Imports the Google Cloud client library
+const {PubSub} = require('@google-cloud/pubsub');
+
+// Creates a client; cache this for further use
+const pubSubClient = new PubSub({
+    projectId: "emisafe",
+    keyFilename: "./key.json"
+});
+// an async function to create a topic
+
+async function listenForMessages(subscriptionNameOrId) {
+    // References an existing subscription
+    console.log("Listening for messages on subscription: ", subscriptionNameOrId);
+    const subscription = pubSubClient.subscription(subscriptionNameOrId);
+
+    // Create an event handler to handle messages
+    let messageCount = 0;
+    const messageHandler = async (message) => {
+        const data = JSON.parse(message.data.toString());
+        console.log(message.attributes);
+        console.log(message.data.toString());
+          // console.log(data);
+
+
+        try {
+            if (message.attributes['notificationType'] === 'ENROLLMENT' && data['state'] === 'PROVISIONING') {
+                try {
+                    const policyId = getRemainingPart(data['policyName'], policyPrefix);
+                    await Device.updateOne({ policy: policyId }, {
+                        name: data['name'],
+                        brand: data['hardwareInfo']['brand'],
+                        model: data['hardwareInfo']['model']
+                    });
+                    console.log(res);
+                } catch (error) {
+                    console.log(error);
+                }
+
+            }
+            message.ack();
+        }
+        catch (e) {
+            console.log(e);
+        }
+
+
+    };
+
+    // Listen for new messages until timeout is hit
+    subscription.on('message', messageHandler);
+    subscription.on('error', (error) => {
+        console.error(error);
+    });
+    subscription.on('listening', () => {
+        console.log("Listening for messages on subscription: ", subscriptionNameOrId);
+    });
+
+  // Wait a while for the subscription to run. (Part of the sample only.)
+    // setTimeout(() => {
+    //     subscription.removeListener('message', messageHandler);
+    //     console.log(`${messageCount} message(s) received.`);
+    // }, timeout);
+}
+
+
+function getRemainingPart(S, b) {
+  // Check if b is a prefix of S
+    if (S.startsWith(b)) {
+    // Get the remaining part after removing b
+        const remainingPart = S.substring(b.length);
+        return remainingPart;
+    } else {
+    // If b is not a prefix, return an appropriate value or handle the error
+    throw 'Substring b is not a prefix of S.';
+    }
+}
+
+
+module.exports = {
+    listenForEnrollments: listenForMessages
+}
