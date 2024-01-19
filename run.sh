276306a1576ca75d212b8bbb7f63bc2f87de2d10
Gautam Bhateja <bhatejagautam1@gmail.com>
2024-01-20 02:10:17 +0530

    Some Change

--- run.sh
+++ run.sh
@@ -6,7 +6,12 @@ git reset --hard origin/main
 
 systemctl daemon-reload
 systemctl restart gunicorn.service
+cd TeenCareWeb
+bash run.sh
 
+cd ..
+cd server_node
+npm install
 pm2 restart TeenCare
 
 echo -n > /root/.pm2/logs/parents-out.log
