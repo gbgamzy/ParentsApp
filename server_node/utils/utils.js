fbd5bf07f499f48eae17030f933ba75fd3ae3f9e
Gautam Bhateja <bhatejagautam1@gmail.com>
2024-03-08 01:56:09 +0530

    Otp Configured

--- server_node/utils/utils.js
+++ server_node/utils/utils.js
@@ -3,14 +3,14 @@ var deviceOtps = new Set();
 function generateRandomWord(length) {
 	const characters =
 		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	let randomWord = '';
+	let randomWord = 'TC-';
 
 	for (let i = 0; i < length; i++) {
 		const randomIndex = Math.floor(Math.random() * characters.length);
 		randomWord += characters.charAt(randomIndex);
     }
     deviceOtps.has(randomWord) ? randomWord = generateRandomWord(length) : deviceOtps.add(randomWord);
-
+	
 	return randomWord;
 }
 
