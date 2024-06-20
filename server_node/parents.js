f4565c981b333fcd2aae7744d7eb0c3870642741
Gautam Bhateja <bhatejagautam1@gmail.com>
2024-06-21 01:59:41 +0530

    Image Size increased

--- server_node/parents.js
+++ server_node/parents.js
@@ -40,9 +40,11 @@ app.use(
 		origin: '*',
 	})
 );
-app.use(express.json());
-app.use(fileUpload());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+
+// Increase the limit for express-fileupload
+app.use(fileUpload({ limits: { fileSize: 5 * 1024 * 1024 } })); // 5MB
 
 app.use('/user', userRoute);
 app.use('/home', homeRoute);
